d64604f80eb1d08988f2dbd0b6f6d14b7ff1cd63
revathijovial <revathijovial27@gmail.com>
2023-06-10 09:26:38 +0530

    search module updated

--- src/Easy_Buy_Module/module1_loginpage.java
+++ src/Easy_Buy_Module/module1_loginpage.java
@@ -9,10 +9,22 @@ public class module1_loginpage {
 		System.out.println("chrome");
 		System.out.println("url");
 		
+		System.out.println("    ");
+		
+	}
+	
+	public static void search_module() {
+		
+		System.out.println("======search_module=======");
+		
+		System.out.println("check given data");
+		System.out.println("check without data");
+		
 	}
 	
 	public static void main(String[] args) {
 		browser();
+		search_module();
 	}
 	
 	

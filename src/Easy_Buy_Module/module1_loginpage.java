a45054318787ab9eb5c4f15d49b480b709df65cb
revathijovial <revathijovial27@gmail.com>
2023-06-10 08:57:10 +0530

    updated module1

---
+++ src/Easy_Buy_Module/module1_loginpage.java
@@ -0,0 +1,20 @@
+package Easy_Buy_Module;
+
+public class module1_loginpage {
+	
+	public static void browser() {
+		
+		System.out.println("======browser launch=======");
+		
+		System.out.println("chrome");
+		System.out.println("url");
+		
+	}
+	
+	public static void main(String[] args) {
+		browser();
+	}
+	
+	
+
+}
